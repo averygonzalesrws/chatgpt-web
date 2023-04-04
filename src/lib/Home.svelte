0a369515d2c0bae96ec8c50a4cc3872a3ef31231
averygonzalesrws <avery.gonzales@retailerwebservices.com>
2023-04-04 15:00:24 -0700

    Additional naming consistency across app

--- src/lib/Home.svelte
+++ src/lib/Home.svelte
@@ -6,10 +6,8 @@
 
 <article class="message">
   <div class="message-body">
-    <strong><a href="https://github.com/Niek/chatgpt-web">ChatGPT-web</a></strong>
-    is a simple one-page web interface to the OpenAI ChatGPT API. To use it, you need to register for
-    <a href="https://platform.openai.com/account/api-keys" target="_blank" rel="noreferrer">an OpenAI API key</a>
-    first. OpenAI bills per token (usage-based), which means it is a lot cheaper than
+    <strong>BanterBuddy</strong>
+    is a simple one-page web interface to the OpenAI ChatGPT API.  OpenAI bills per token (usage-based), which means it is a lot cheaper than
     <a href="https://openai.com/blog/chatgpt-plus" target="_blank" rel="noreferrer">ChatGPT Plus</a>, unless you use
     more than 10 million tokens per month. All messages are stored in your browser's local storage, so everything is
     <strong>private</strong>. You can also close the browser tab and come back later to continue the conversation.
